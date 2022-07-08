29fd78b330d110a509ebf0be41c8b313da761779
Boyang Gao <boyang.gao@yahoo.com>
2022-07-07 23:55:13 -0400

    feat: finished wrap class for lab2

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + n % (1UL << 32);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,26 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    auto raw_v = n - isn;
+    uint64_t start = 0;
+    uint64_t end = (1UL << 32) - 1;
+    while (start + 1 < end) {
+        auto mid = start + (end - start) / 2;
+        auto mid_raw = mid * (1UL << 32) + raw_v;
+        if (checkpoint > mid_raw) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    auto start_raw = start * (1UL << 32) + raw_v;
+    auto end_raw = end * (1UL << 32) + raw_v;
+    auto start_diff = max(start_raw, checkpoint) - min(start_raw, checkpoint);
+    auto end_diff = max(end_raw, checkpoint) - min(end_raw, checkpoint);
+    if (start_diff < end_diff) {
+        return start_raw;
+    } else {
+        return end_raw;
+    }
 }
+
